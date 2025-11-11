b8d8f3c7212026c97b053086fe4c277460656bc3
Sarvesh Kumar Singh <sarveshsingh8303744279@gmail.com>
2025-11-11 11:50:38 +0530

    add variables i  utils file

--- utils/util.js
+++ utils/util.js
@@ -5,3 +5,10 @@ function greet(name) {
 
 console.log(greet("Sarvesh"));
 
+
+// variables.js
+let name = "Sarvesh";
+const age = 22;
+let city = "Lucknow";
+
+console.log(`My name is ${name}, I am ${age} years old, and I live in ${city}.`);
